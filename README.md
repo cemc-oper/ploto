7d4827728165465ff18ead324c89db47ffd1d567
perillaroc <perillaroc@gmail.com>
2018-03-20 14:27:35 +0800

    change gitignore and add README.md.

---
+++ README.md
@@ -0,0 +1,3 @@
+# gidat-plot
+
+GIDAT plot.
---
+++ setup.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from setuptools import setup
+
+setup(
+    name='gidat-plot',
+
+    version='0.1',
+
+    description='GIDAT plot.',
+    long_description=__doc__,
+
+    packages=['gidat_plot'],
+
+    include_package_data=True,
+
+    zip_safe=False,
+
+    install_requires=[
+        'click',
+        'pyyaml',
+        'kafka-python'
+    ]
+)
