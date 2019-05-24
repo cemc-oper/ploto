bb59829a741fdb21bd69cbc1b1f2c6a463e6ad9e
perillaroc <perillaroc@gmail.com>
2019-05-24 09:41:50 +0800

    :construction: add ice_area but not tested.

--- ploto_server/common/esmdiag/metrics/climo/figures/ice_area.py
+++ ploto_server/common/esmdiag/metrics/climo/figures/ice_area.py
@@ -4,10 +4,10 @@ ice area
 
 data requires:
     aice.monthly
-    hi.monthly
-
 """
-from ploto_server.common.esmdiag.metrics.climo.figures import get_plotter_step
+import datetime
+
+from ploto_server.common.esmdiag.metrics.climo.util import get_plotter_step, get_gw_step
 
 
 def generate_figure_task(figure_config, common_config) -> dict:
@@ -35,13 +35,72 @@ def generate_figure_task(figure_config, common_config) -> dict:
         }
     :return:
     """
-    steps = [
+    steps = []
+
+    start_date = datetime.datetime.strptime(common_config['date']['start'], "%Y-%m-%d")
+    end_date = datetime.datetime.strptime(common_config['date']['end'], "%Y-%m-%d")
+    date_range = [start_date.strftime("%Y%m%d"), end_date.strftime("%Y%m%d")]
+
+    file_prefix = '{ice_id}.{case_id}'.format(
+        ice_id=common_config['model_info']['ice_id'],
+        case_id=common_config['case_info']['id']
+    )
+
+    step1_file_prefix = '{file_prefix}.step1'.format(
+        file_prefix=file_prefix
+    )
+
+    step1_fields = [
+        'CICE'
+    ]
+
+    time_range_string = "{start_date}:{end_date}".format(
+        start_date=common_config['date']['start'],
+        end_date=common_config['date']['end'],
+    )
+    output_file_pattern = "{file_prefix}.{name}.monthly.{time_range}.nc"
+
+    steps.extend([
         {
             'step_type': 'fetcher',
-            'type': 'local_fetcher',
             'common': common_config,
+            'type': 'edp_fetcher',
+            'query_param': {
+                'type': 'nc',
+                'output_dir': './data',
+                'file_prefix': step1_file_prefix,
+                'date_range': date_range,
+                'field_names': step1_fields,
+                'datedif': 'h0'
+            },
         },
-        get_plotter_step(figure_config, common_config)]
+    ])
+
+    steps.extend(
+        [
+            {
+                'step_type': 'processor',
+                'type': 'cdo_processor',
+                'operator': 'select',
+                'params': {
+                    'name': field,
+                    'startdate': common_config['date']['start'],
+                    'enddate': common_config['date']['end']
+                },
+                'input_files': [
+                    './data/{step1_file_prefix}.*.nc'.format(step1_file_prefix=step1_file_prefix)
+                ],
+                'output_file': output_file_pattern.format(
+                    file_prefix=file_prefix,
+                    time_range=time_range_string,
+                    name=field,
+                ),
+            } for field in step1_fields
+        ]
+    )
+
+    # steps.extend(get_gw_step(figure_config, common_config))
+    steps.append(get_plotter_step(figure_config, common_config))
 
     task = {
         'steps': steps
