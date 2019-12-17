ef8fbf08aede62a5e6e4b99031af3867ab1f35c9
perillaroc <perillaroc@gmail.com>
2019-12-17 19:49:26 +0800

    :alembic: dagbag is not working.

--- ploto-airflow/ploto_airflow/dags/fetcher_dagbag.py
+++ ploto-airflow/ploto_airflow/dags/fetcher_dagbag.py
@@ -7,12 +7,15 @@ from ploto_airflow import MODULE_PATH
 
 
 dag_dirs = [
-    str(pathlib.Path(MODULE_PATH, "fetcher/edp_fetcher"))
+    str(pathlib.Path(MODULE_PATH, "fetcher/edp_fetcher")),
+    str(pathlib.Path(MODULE_PATH, "fetcher/edp_fetcher_bash"))
 ]
 
 for a_dir in dag_dirs:
+    print(a_dir)
     dag_bag = DagBag(a_dir)
 
     if dag_bag:
         for dag_id, dag in dag_bag.dags.items():
+            print(dag_id, dag)
             globals()[dag_id] = dag
