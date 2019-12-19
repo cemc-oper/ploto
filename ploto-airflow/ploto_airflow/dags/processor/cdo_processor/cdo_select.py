a65de14ebe68947d3a21756d0990b50025a7ccaa
perillaroc <perillaroc@gmail.com>
2019-12-19 09:56:57 +0800

    :sparkles: add cdo select dag.

---
+++ ploto-airflow/ploto_airflow/dags/processor/cdo_processor/cdo_select.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+"""
+Run the following command to test:
+   airflow trigger_dag \
+    --conf "$(jq '.' ./example.json)" \
+    ploto_processor_cdo_select
+"""
+import airflow.utils.dates
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+
+from ploto_airflow.processor.cdo_processor.select import generate_operator as generate_cdo_select_operator
+
+
+args = {
+    "owner": "ploto",
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+
+dag_id = "ploto_processor_cdo_select"
+
+
+with DAG(
+    dag_id=dag_id,
+    default_args=args,
+    schedule_interval=None,
+) as dag:
+    show_task = BashOperator(
+        task_id="show_date",
+        bash_command='date',
+        dag=dag,
+    )
+
+    run_task_id = "run_cdo_select"
+    run_task = generate_cdo_select_operator(run_task_id)
+    run_task.dag = dag
+
+    run_task.set_upstream(show_task)
